a9505da29fc0390b87b63ced9b0afc68609760e9
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-02-16 16:11:43 +0200

    sidebar

---
+++ website/js/index.js
@@ -0,0 +1,9 @@
+function openFunction(){
+    document.getElementById("menu").style.width="300px";
+    document.getElementById("mainbox").style.marginLeft="300px";
+}
+
+function closeFunction(){
+    document.getElementById("menu").style.width="0px";
+    document.getElementById("mainbox").style.marginLeft="300px";
+}
