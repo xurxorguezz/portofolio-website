5b0c07efd2ef5257bc1bdb31978fa482f9692aaa
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-03-17 16:20:36 +0200

    created react app

---
+++ react-website/src/Components/App.jsx
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import '../App.css';
+
+function App() {
+
+  console.log("lmao nice");
+
+  return (
+    <div>
+      Hello world
+    </div>
+  );
+}
+
+export default App;
