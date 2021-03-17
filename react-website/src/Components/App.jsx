b1dd6ad0e290ed61dc123f275a766199c27c0291
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-03-17 23:05:29 +0200

    real hard button to make

--- react-website/src/Components/App.jsx
+++ react-website/src/Components/App.jsx
@@ -5,8 +5,23 @@ import { useState } from 'react';
 
 
 function App() {
+
+  function handleExplore(){
+    //COMPONENT NEEDED HERE
+  }
+
   return (
-    <img src={mainImg} style={{width: '100%', height: '100%'}}/>
+    <div className="image">
+      <img src={mainImg} style={{width: '100%', height: '100%'}}/>
+      <div className="small-about">
+        <h2>Arin Grigoras
+        <p style={{fontSize: '15px'}}>Selft-taugh programmer.<br/>Intermediate in C.<br/>Learning React.js</p></h2>
+        <div className="wrapper">
+          <button onClick={handleExplore} className="explore" style={{color: 'black'}}>Explore</button>
+        </div>
+      </div>
+    </div>
+
   );
 }
 
