b36620c76b9dbaf45257c376836ceb601298244d
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-03-17 23:50:44 +0200

    OMAIGAD IT GODDAMN WORKS

--- react-website/src/Components/App.jsx
+++ react-website/src/Components/App.jsx
@@ -2,25 +2,31 @@ import mainImg from '../pics/mainImg.jpg';
 import '../App.css';
 import React from 'react';
 import { useState } from 'react';
+import Explore from './Explore';
+import ReactDOM from 'react-dom';
 
 
 function App() {
 
   function handleExplore(){
-    //COMPONENT NEEDED HERE
+    ReactDOM.render(
+      <Explore />,
+      document.getElementById('root')
+    );
   }
 
   return (
+
     <div className="image">
-      <img src={mainImg} style={{width: '100%', height: '100%'}}/>
-      <div className="small-about">
-        <h2>Arin Grigoras
-        <p style={{fontSize: '15px'}}>Selft-taugh programmer.<br/>Intermediate in C.<br/>Learning React.js</p></h2>
-        <div className="wrapper">
-          <button onClick={handleExplore} className="explore" style={{color: 'black'}}>Explore</button>
-        </div>
+    <img src={mainImg} style={{width: '100%', height: '100%'}}/>
+    <div className="small-about">
+      <h2>Arin Grigoras
+      <p style={{fontSize: '15px'}}>Selft-taugh programmer.<br/>Intermediate in C.<br/>Learning React.js</p></h2>
+      <div className="wrapper">
+        <button onClick={handleExplore} className="explore" style={{color: 'black'}}>Explore</button>
       </div>
     </div>
+  </div>
 
   );
 }
---
+++ react-website/src/Components/Explore.jsx
@@ -0,0 +1,16 @@
+//import { render } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function Explore(){
+    return (
+        <p>
+            IT WORKED
+        </p>
+    );
+}
+
+
+
+export default Explore;
