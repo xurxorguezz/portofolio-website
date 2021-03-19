4830aeb73fce39aaabcceb222d3e41101680bd84
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-03-19 22:28:57 +0200

    made a little bit of the about section

--- react-website/src/Components/About.jsx
+++ react-website/src/Components/About.jsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import Top2 from './Top-about.jsx';
-import '../css/About.css';
+import '../App.css';
+//import '../css/About.css';
 
 export default function About(){
     return (
         <div className="about">
             <Top2 />
 
-            <div className="information-right">
-                
+            <br/>
+
+            <div className="description">
+                <p><h1 style={{font: 'fantasy', fontSize: '80px'}}>HELLO;</h1></p>
+                <div className="about"></div>
+            </div>
+
+            <div className="right-info">
+                <div className="vl"></div>
+
+                <div className="like">
+                    <p><h3>Things I like:</h3></p>
+                    <ul>
+                        <li>
+                            <a href="https://github.com/koreaneggroll/libavoidrepetition-c" style={{textDecoration: 'none', color: 'black'}}>Contributing to Open-Source</a>
+                        </li>
+
+                        <br/>
+
+                        <li>
+                            Doggos
+                        </li>
+
+                        <br/>
+
+                        <li>
+                            Learning new things
+                        </li>
+                    </ul>
+                </div>
+
             </div>
         </div>
     );
